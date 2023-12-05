3faac339077120fb3efc56a9af1e4afaeabde4fd
valonmulolli <valonmulolli@gmail.com>
2023-12-05 18:20:32 +0100

    autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,22 @@
+return {
+  'windwp/nvim-autopairs',
+  enabled = require("plugins.treesitter").enabled,
+  opts = {
+      map_cr = false,
+      check_ts = true,
+      fast_wrap = {
+          map = '<M-e>',
+          chars = { '{', '[', '(', '"', "'" },
+          pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+          end_key = '$',
+          keys = 'qwertyuiopzxcvbnmasdfghjkl',
+          check_comma = true,
+          highlight = 'Search',
+          highlight_grey='Comment'
+      },
+  },
+  config = function(_, opts)
+      local npairs = require('nvim-autopairs')
+      npairs.setup(opts)
+  end,
+}
