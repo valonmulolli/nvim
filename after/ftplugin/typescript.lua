77b39c095d73d018204b6ecc96e0b7822b648b97
valonmulolli <valonmulolli@gmail.com>
2025-06-14 00:22:34 +0200

    feat: add TypeScript configuration
    - Add TypeScript filetype settings
    - Add TypeScript server configuration
    - Configure TypeScript LSP with proper settings

---
+++ after/ftplugin/typescript.lua
@@ -0,0 +1,7 @@
+-- TypeScript filetype settings
+
+-- Use 2 spaces for indentation (matching JavaScript)
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2
---
+++ lua/plugins/lsp/servers/tsserver.lua
@@ -0,0 +1,52 @@
+---@type LazySpec
+---@class LazySpec
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          -- Lazy-load on filetype
+          autostart = false,
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern("tsconfig.json", "jsconfig.json", ".git")(fname)
+                or util.path.dirname(fname)
+          end,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
